7f51f28a18284a4f32791724f8fe10f83098020b
Steven Chan <schan@rihousing.com>
2022-01-28 13:44:54 -0500

    Update Header, App, Home, and Footer by adding Routes and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.scss';
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="app">
+      <Router>
+        <Header></Header>
+        
+        <div className="container">
+          <Routes>
+           <Route path="/" exact element={<Home/>}/>
+           <Route path="/movie/:imdbID" exact element={<MovieDetails/>}/>
+           <Route path="*" exact element={<PageNotFound/>}/>
+          </Routes>
+        </div>
+       
+
+        <Footer/>
+      </Router>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import MovieListing from "../MovieListing/MovieListing";
+import "./Home.scss"
+import homeIcon from "../../images/popcorn.png"
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className='banner-img'>
+        <img src={homeIcon}></img>
+      </div>
+      <MovieListing/>
+    </div>
+    
+  );
 };
 
 export default Home;
